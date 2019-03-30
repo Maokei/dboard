c02e9d91b54317c5a17a855891e01dd51da08da0
maokei <rickard.joh@gmail.com>
2019-03-30 11:36:23 +0100

    updated config

--- dboard-server/src/main/java/se/maokei/dboard_server/LaunchVerticle.java
+++ dboard-server/src/main/java/se/maokei/dboard_server/LaunchVerticle.java
@@ -1,9 +1,7 @@
 package se.maokei.dboard_server;
 
 import io.vertx.config.ConfigRetriever;
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.DeploymentOptions;
-import io.vertx.core.Vertx;
+import io.vertx.core.*;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
@@ -21,5 +19,75 @@ public class LaunchVerticle extends AbstractVerticle {
         vertx.deployVerticle(new LaunchVerticle(), options);
       }
     });
+  }
+
+  /**
+   * @param future
+   * */
+  @Override
+  public void start(Future<Void > future) {
+    CompositeFuture.all(deployHelper(vertx, MainVerticle.class.getName()),
+      deployHelper(vertx, DockerVerticle.class.getName()))
+      .setHandler(result -> {
+        if(result.succeeded()){
+          future.complete();
+        } else {
+          future.fail(result.cause());
+          LOGGER.error("Failed to deploy verticles!" + result.cause());
+        }
+      });
+  }
+
+  private Future<Void> deployHelper(Vertx vertx, String name) {
+    final Future<Void> future = Future.future();
+    ConfigRetriever cr = ConfigRetriever.create(vertx);
+    cr.getConfig(config -> {
+      if (config.succeeded()) {
+        JsonObject conf = config.result();
+        System.out.println("Starting server with configuration: \n" + conf.encodePrettily());
+        DeploymentOptions options = new DeploymentOptions().setConfig(conf);
+        vertx.deployVerticle(name, options, result -> {
+          if (result.failed()) {
+            LOGGER.error("Failed to deploy verticle " + name);
+            future.fail(result.cause());
+          } else {
+            LOGGER.info("Deployed verticle " + name);
+            future.complete();
+          }
+        });
+      }
+    });
+    return future;
+  }
+
+  /**
+   * Deploys verticle with class name loads config.json and clusters with hazelcast.
+   * @param name class name of verticle to be deployed.
+   * @return Future
+   * */
+  private Future<Void> deploymentHelperSeprate(String name) {
+    final Future<Void> future = Future.future();
+    VertxOptions vertxOptions = new VertxOptions();
+    vertxOptions.setClustered(true);
+    Vertx.clusteredVertx(vertxOptions, res -> {
+      if(res.succeeded()) {
+        Vertx vertx  = res.result();
+        ConfigRetriever cr = ConfigRetriever.create(vertx);
+        cr.getConfig(config -> {
+          if(config.succeeded()) {
+            JsonObject conf = config.result();
+            DeploymentOptions options = new DeploymentOptions().setConfig(conf);
+            vertx.deployVerticle(name, options, result -> {
+              if(result.succeeded()) {
+                future.complete();
+              }else{
+                future.fail(result.cause());
+              }
+            });
+          }
+        });
+      }
+    });
+    return future;
   }
 }
