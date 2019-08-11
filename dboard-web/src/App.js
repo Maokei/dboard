4bcde869d866d2366a38379557ed0e0ae030b319
maokei <rickard.joh@gmail.com>
2019-08-11 12:44:01 +0200

    Header

--- dboard-web/src/App.js
+++ dboard-web/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './components/common/header/Header';
 import './App.css';
 
 function App() {
   return (
-   <div></div>
+      <div>
+        <Header/>
+        <div className="">Body</div>
+      </div>
   );
 }
 
