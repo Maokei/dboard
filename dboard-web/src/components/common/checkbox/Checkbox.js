6f89b074a13e80f1fcba447c6f2b1298eee48374
maokei <rickard.joh@gmail.com>
2019-08-12 23:53:37 +0200

    Checkbox component

---
+++ dboard-web/src/components/common/checkbox/Checkbox.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Checkbox.css';
+
+const Checkbox = () => {
+    return(
+        <div>
+            <input className="Checkbox-switch"
+                id={`switch-new`}
+                type="checkbox"
+            />
+            <label className="Checkbox-switch-label"
+                htmlFor={`Checkbox-switch`}
+            >
+                <span className={`Checkbox-switch-button`} />
+            </label>
+        </div>
+    );
+}
+
+export default Checkbox;
