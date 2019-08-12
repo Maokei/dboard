86f99566e579d6ecc93038cb2c0f45f10f4c6348
Shirley Wang <shirleywang527@gmail.com>
2019-08-12 07:25:53 +0200

    fix arrow animation and viewport height

--- dboard-web/src/components/common/sidebar/Sidebar.js
+++ dboard-web/src/components/common/sidebar/Sidebar.js
@@ -8,20 +8,25 @@ class Sidebar extends Component {
 
         this.state = {
             visible: false,
+            flip: false
         }
         this.toggleMenu = this.toggleMenu.bind(this);
     }
 
     toggleMenu() {
         console.log("clicked");
-        this.setState({visible: !this.state.visible});
+        this.setState({
+            visible: !this.state.visible,
+            flip: !this.state.flip
+        });
     }
 
     render() {
         const showClass = this.state.visible ? 'show profile' : 'profile';
+        const flip180 = this.state.flip ? 'flip-180 fa fa-arrow-right' : 'fa fa-arrow-right';
         return(
             <div className="sidebar-container">
-                <button className="toggle" onClick={this.toggleMenu}><i className="fa fa-arrow-right"></i></button>
+                <button className="toggle" onClick={this.toggleMenu}><i className={flip180}></i></button>
                 <div className={showClass}>
                     <img src={avatarImg} className="profile-img" alt="profile image"></img>
                     <h3 className="profile-title">Title</h3>
