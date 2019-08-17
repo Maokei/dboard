0c0666b0b0310bb4b6f8afd3bebd4fa99f390d55
Rickard Johansson <rickard.joh@gmail.com>
2019-08-17 11:39:51 +0200

    Switched to sass

--- dboard-web/src/App.js
+++ dboard-web/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/common/header/Header';
 import Sidebar from './components/common/sidebar/Sidebar'
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
