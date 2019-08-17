ca677000e8368733a7e4e6429c07c5da65d670ff
Rickard Johansson <rickard.joh@gmail.com>
2019-08-17 12:56:09 +0200

    Updated demo front-end

--- dboard-server/src/main/resources/webroot/main.js
+++ dboard-server/src/main/resources/webroot/main.js
@@ -4,16 +4,19 @@ function generateHtml(items) {
     items.forEach(item => {
         const tr = document.createElement('tr');
         tr.classList.add('row-item');
+        var ports = item.Ports[0]
         tr.innerHTML = `
             <td>${item.Id}</td>
-            <td><span>Description: ${item.Names}</span></td>
-            <td><span>Image: ${item.Image}</span></td>
-            <td><span>Command: ${item.Command}</span></td>
-            <td><span>Created: ${item.Created}</span></td>
-            <td><span>Created: ${item.Status}</span></td>
-            <td><span>Created: ${item.Ports}</span></td>
+            <td><span>${item.Names}</span></td>
+            <td><span>${item.Image}</span></td>
+            <td><span>${item.Command}</span></td>
+            <td><span>${item.Created}</span></td>
+            <td><span>${item.Status}</span></td>
+            <td><span>${ports.PrivatePort}</span></td>
+            <td><span>${ports.PublicPort}</span></td>
+            <td><span>${ports.Type}</span></td>
         `;
-        console.log(tr.innerHTML);
+        console.log(JSON.stringify(item));
         output.appendChild(tr);
     });
 }
@@ -25,7 +28,7 @@ setInterval(() => {
     xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var containerList = JSON.parse(this.responseText);
-        generateHtml(containerList);
+            generateHtml(containerList);
         }
     };
 
