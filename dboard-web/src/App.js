4c59f3d5a4bf5039ab58ad8b646bce8d3a598d0d
Rickard Johansson <rickard.joh@gmail.com>
2019-08-24 12:24:53 +0200

    toggle

--- dboard-web/src/App.js
+++ dboard-web/src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/common/header/Header';
 import Sidebar from './components/common/sidebar/Sidebar'
+import Toggle from './components/common/toggle/Toggle';
 import './App.scss';
 
 function App() {
   return (
       <div>
         <Header/>
-        <div className=""></div>
         <Sidebar />
+        <div>
+        <Toggle shape={false} defState={false}/>
+        <Toggle shape={true} defState={true} handleToggle={() => console.log("asdas")}/>
+        </div>
       </div>
   );
 }
