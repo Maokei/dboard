e1dcc718b6b078bf55fb1c30cdf5c111e3e7ac87
Shirley Wang <shirleywang527@gmail.com>
2019-09-07 21:33:23 +0200

    show texts function

--- dboard-web/src/components/common/sidebar/Sidebar.js
+++ dboard-web/src/components/common/sidebar/Sidebar.js
@@ -8,7 +8,8 @@ class Sidebar extends Component {
 
         this.state = {
             visible: false,
-            flip: false
+            flip: false,
+            addWidth: false
         }
         this.toggleMenu = this.toggleMenu.bind(this);
     }
@@ -17,28 +18,26 @@ class Sidebar extends Component {
         console.log("clicked");
         this.setState({
             visible: !this.state.visible,
-            flip: !this.state.flip
+            flip: !this.state.flip,
+            addWidth: !this.state.width
         });
     }
 
     render() {
-        const showClass = this.state.visible ? 'show profile' : 'profile';
+        const showClass = this.state.visible ? 'show profile__details' : 'profile__details';
+        const showClass2 = this.state.visible ? 'show sidebar__menu-desc' : 'sidebar__menu-desc';
+        const sidebarWidth = this.state.addWidth ? 'sidebar-container-wide' : 'sidebar-container'
         const flip180 = this.state.flip ? 'flip-180 fa fa-arrow-right' : 'fa fa-arrow-right';
         return(
-            <div className="sidebar-container">
+            <div className={sidebarWidth}>
                 <div className="profile">
                     <a href="#">
                         <img src={avatarImg} className="profile-thumbnail" alt="profile image thumbnail"></img>
                     </a>
-                    <h3 className="profile-title">Jane Doe</h3>
-                    <p className="profile-desc">Location Country</p>
+                    <h3 className={showClass}>Jane Doe</h3>
+                    <p className={showClass}>Location Country</p>
                 </div>
                 <button className="toggle" onClick={this.toggleMenu}><i className={flip180}></i></button>
-                {/* <div className={showClass}>
-                    <img src={avatarImg} className="profile-img" alt="profile image"></img>
-                    <h3 className="profile-title">Jane Doe</h3>
-                    <p className="profile-desc">Location Country</p>
-                </div> */}
                 <div className="sidebar__menu">
                     <div className="sidebar__menu-icons">
                         <a href="#"><i class="fas fa-home"></i></a>
@@ -47,7 +46,7 @@ class Sidebar extends Component {
                         <a href="#"><i class="fas fa-crown"></i></a>
                         <a href="#"><i class="fas fa-cog"></i></a>
                     </div>
-                    <div className="sidebar__menu-desc">
+                    <div className={showClass2}>
                         <span>Home</span>
                         <span>History</span>
                         <span>Library</span>
